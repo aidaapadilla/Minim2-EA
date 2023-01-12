bf9a2c44827bb79f6dc63693bffdba774871efba
Aida Padilla <aida.padilla.gonzalez.aiko@gmail.com>
2023-01-12 19:47:40 +0100

    minim2

--- EA-Frontend/flutter_front/lib/views/report.dart
+++ EA-Frontend/flutter_front/lib/views/report.dart
@@ -62,6 +62,7 @@ class _MyReports extends State<MyReports> {
     Issue(id: 2, name: "User"),
     Issue(id: 3, name: "Other")
   ];
+
   var isLoaded = false;
   List<Issue> _selected = [];
   List<Report>? reports = [];
@@ -92,6 +93,23 @@ class _MyReports extends State<MyReports> {
 
   @override
   Widget build(BuildContext context) {
+    void submit() {
+      Navigator.of(context, rootNavigator: true).pop();
+    }
+
+    Future openDialog(String text) => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text(text),
+            actions: [
+              TextButton(
+                onPressed: submit,
+                child: const Text('Ok'),
+              ),
+            ],
+          ),
+        );
+
     return MaterialApp(
         home: Scaffold(
       drawer: const DrawerScreen(),
@@ -101,122 +119,121 @@ class _MyReports extends State<MyReports> {
         backgroundColor: Colors.blueGrey,
       ),
       body: SingleChildScrollView(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-            const ListTile(
-              leading: const Icon(Icons.highlight_remove_sharp),
-              title: Text(
-                  'Welcome to the report zone, here you can contact with the Service client to inform or report an issue',
-                  style: TextStyle(fontWeight: FontWeight.w900)),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+                  Widget>[
+        const ListTile(
+          leading: const Icon(Icons.highlight_remove_sharp),
+          title: Text(
+              'Welcome to the report zone, here you can contact with the Service client to inform or report an issue',
+              style: TextStyle(fontWeight: FontWeight.w900)),
+        ),
+        MultiSelectDialogField(
+          items: _issueType.map((e) => MultiSelectItem(e, e.name)).toList(),
+          listType: MultiSelectListType.CHIP,
+          onConfirm: (values) {
+            _selected = values;
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.access_alarms_rounded),
+          title: TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Please write your issue',
+              border: OutlineInputBorder(),
+              contentPadding: EdgeInsets.symmetric(vertical: 40),
             ),
-            MultiSelectDialogField(
-              items: _issueType.map((e) => MultiSelectItem(e, e.name)).toList(),
-              listType: MultiSelectListType.CHIP,
-              onConfirm: (values) {
-                _selected = values;
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.access_alarms_rounded),
-              title: TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Please write your issue',
-                  border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.symmetric(vertical: 40),
-                ),
-                controller: textController,
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
+            controller: textController,
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+        ),
+        const ListTile(
+          leading: const Icon(Icons.balance),
+          title: Text(
+              'Select the gravity of the issue, where 3 is a big issue and 0 is a low issue',
+              style: TextStyle(fontWeight: FontWeight.w500)),
+        ),
+        Slider(
+          value: sliderDif,
+          onChanged: (newScore) {
+            setState(() {
+              sliderDif = newScore;
+            });
+          },
+          min: 0.0,
+          max: 3.0,
+          divisions: 3,
+          activeColor: Colors.blueGrey,
+          inactiveColor: Colors.blueGrey.shade100,
+          thumbColor: Colors.blueGrey,
+          label: "$sliderDif",
+        ),
+        const Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30.0),
+          child: ElevatedButton(
+              onPressed: () {
+                if (textController.text.isNotEmpty && _selected.isNotEmpty) {
+                  String textW = textController.text.toString();
+                  int difficulty = int.parse(sliderDif.toString());
+                  String now = new DateTime.now().toString();
+                  print(now);
+                  String typeSel = "";
+                  while (_selected.isNotEmpty) {
+                    typeSel = typeSel + "/" + _selected.last.name.toString();
+                    _selected.removeLast();
                   }
-                  return null;
-                },
-              ),
-            ),
-            const ListTile(
-              leading: const Icon(Icons.balance),
-              title: Text(
-                  'Select the gravity of the issue, where 3 is a big issue and 0 is a low issue',
-                  style: TextStyle(fontWeight: FontWeight.w500)),
-            ),
-            Slider(
-              value: sliderDif,
-              onChanged: (newScore) {
-                setState(() {
-                  sliderDif = newScore;
-                });
+                  print(typeSel);
+                  Report p = Report(
+                      //user_id:  StorageAparcam().getId(),
+                      id: '',
+                      user: '',
+                      type: typeSel,
+                      text: textW,
+                      level: difficulty);
+
+                  ReportServices().createReport(p);
+                  //openDialog("Parking created.");
+                  setState(() {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const ListParkings()));
+                  });
+
+                  //String formatter =
+                  //DateFormat('yMd').format(now); // 28/03/2020
+                } else {
+                  openDialog("Please make sure that all fields are complete");
+                }
+
+                ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(65, 143, 74, 163));
               },
-              min: 0.0,
-              max: 3.0,
-              divisions: 3,
-              activeColor: Colors.blueGrey,
-              inactiveColor: Colors.blueGrey.shade100,
-              thumbColor: Colors.blueGrey,
-              label: "$sliderDif",
-            ),
-            const Divider(),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30.0),
-              child: ElevatedButton(
-                  onPressed: () {
-                    if (textController.text.isNotEmpty &&
-                        _selected.isNotEmpty) {
-                      String textW = textController.text.toString();
-                      int difficulty = int.parse(sliderDif.toString());
-                      String now = new DateTime.now().toString();
-                      print(now);
-                      String typeSel = "";
-                      while (_selected.isNotEmpty) {
-                        typeSel =
-                            typeSel + "/" + _selected.last.name.toString();
-                        _selected.removeLast();
-                      }
-                      print(typeSel);
-                      Report p = Report(
-                          //user_id:  StorageAparcam().getId(),
-                          id: '',
-                          user: '',
-                          type: typeSel,
-                          text: textW,
-                          level: difficulty);
-
-                      ReportServices().createReport(p);
-                      //openDialog("Parking created.");
-                      setState(() {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => const ListParkings()));
-                      });
-
-                      //String formatter =
-                      //DateFormat('yMd').format(now); // 28/03/2020
-                    } else {}
-
-                    ElevatedButton.styleFrom(
-                        backgroundColor:
-                            const Color.fromARGB(65, 143, 74, 163));
-                  },
-                  child: const Text('Submit')),
-            ),
-            // ListView.builder(
-            //   itemCount: reports?.length,
-            //   itemBuilder: (context, index) {
-            //     return Card(
-            //       color: const Color.fromARGB(255, 144, 180, 199),
-            //       child: ListTile(
-            //         leading: Container(
-            //           width: 100,
-            //           height: 100,
-            //           //child: Image.asset('parking1.jpg'),
-            //         ),
-            //         title: Text('${reports![index].type}'),
-            //         subtitle: Text(
-            //             'Level: ${reports![index].level}'), //|| Issue: ${reports![index].text}'),
-            //       ),
-            //     );
-            //   },
-            // )
-          ])),
+              child: const Text('Submit')),
+        ),
+        // ListView.builder(
+        //   itemCount: reports?.length,
+        //   itemBuilder: (context, index) {
+        //     return Card(
+        //       color: const Color.fromARGB(255, 144, 180, 199),
+        //       child: ListTile(
+        //         leading: Container(
+        //           width: 100,
+        //           height: 100,
+        //           //child: Image.asset('parking1.jpg'),
+        //         ),
+        //         title: Text('${reports![index].type}'),
+        //         subtitle: Text(
+        //             'Level: ${reports![index].level}'), //|| Issue: ${reports![index].text}'),
+        //       ),
+        //     );
+        //   },
+        // )
+      ])),
     ));
   }
 }
